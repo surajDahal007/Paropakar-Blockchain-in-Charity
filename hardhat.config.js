c5046d78aed6834abb6c457959b4b3e64a82cdcf
Ujwal Karki <ujwal.bdev@gmail.com>
2023-01-27 21:30:25 +0545

    added polygon network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  networks:{
-    polygon:{
-      url:"",
-      accounts:""
-    }
-  }
+  networks: {
+    polygon: {
+      url: process.env.url,
+      accounts: [process.env.private_key],
+    },
+  },
 };
