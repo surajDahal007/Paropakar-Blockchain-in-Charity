b3d2c8a83c896b28b477f9b8fb8a082885f6a7e3
Ujwal Karki <ujwal.bdev@gmail.com>
2023-01-27 20:24:06 +0545

    added web3 button

--- frontend/components/ui/Navbar.js
+++ frontend/components/ui/Navbar.js
@@ -1,17 +1,19 @@
-import styles from "./Navbar.module.css"
-import Wallet from "../modal/Wallet";
+import styles from "./Navbar.module.css";
 import Link from "next/link";
+import { Web3Button } from "@web3modal/react";
 
 const Navbar = () => {
-  return( 
-    <div className={styles.main}>
+  return (
+    <div>
       <Link href="/" className={styles.link}>
-        <font style={{fontFamily:"Monaco"}}> Paropakar </font> : <i> Blockchain in Charity </i>
-      </Link>  
-      <Wallet />
-      
+        <font style={{ fontFamily: "Monaco" }}> Paropakar </font> :{" "}
+        <i> Blockchain in Charity </i>
+      </Link>
+      <div className={styles.connectButton}>
+        <Web3Button />
+      </div>
     </div>
-  )
+  );
 };
 
 export default Navbar;
