e97c80e1aed4b9a4fc6ce7e7ccd89386aa6a49b3
Ujwal Karki <ujwal.bdev@gmail.com>
2023-01-27 20:23:17 +0545

    configured web3modal and wagmiclient provider

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,57 @@
 import "../styles/globals.css";
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+
+import { Web3Modal } from "@web3modal/react";
+
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+
+import { goerli, polygon } from "wagmi/chains";
 
 //import  navbar component here
 import Navbar from "../components/ui/Navbar";
 import Footer from "../components/ui/Footer";
 
-function MyApp({ Component, pageProps }) { 
-  return( 
+const chainSupport = [goerli, polygon];
+
+// create wagmi client to configure chains to get connected
+/**
+ {@param} suppoted chains and provider node to be used
+ {@returns} object
+ {@description} destructure retirned object to get provider obj
+ */
+const { provider } = configureChains(chainSupport, [
+  walletConnectProvider({ projectId: "1feab24f8df3e7c942161253e25657ce" }),
+]);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: "Paropakar", chainSupport }),
+  provider,
+});
+
+// Web3Modal Ethereum Client
+const ethereumClient = new EthereumClient(wagmiClient, chainSupport);
+
+function MyApp({ Component, pageProps }) {
+  return (
     <>
       <Navbar />
-      <Component {...pageProps} />
-        <Footer />
+      <WagmiConfig client={wagmiClient}>
+        <Component {...pageProps} />
+      </WagmiConfig>
+
+      <Footer />
+
+      <Web3Modal
+        projectId="1feab24f8df3e7c942161253e25657ce"
+        ethereumClient={ethereumClient}
+      />
     </>
-    )
+  );
 }
 
 export default MyApp;
