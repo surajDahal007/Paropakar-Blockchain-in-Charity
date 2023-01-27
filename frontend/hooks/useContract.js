961591809d081da14c08b5e4587f6901daaf91d9
Ujwal Karki <ujwal.bdev@gmail.com>
2023-01-27 21:32:45 +0545

    exports two custom contract hooks

--- frontend/hooks/useContract.js
+++ frontend/hooks/useContract.js
@@ -1,7 +1,43 @@
-import React from "react";
+import { useContract, useProvider, useSigner } from "wagmi";
+import { campaignAbi, factoryAbi, factoryAddress } from "../constants";
 
-const useContract = () => {
-  return <div>useContract</div>;
+/**
+ * @description applied for both custom hook
+ * @param {*} isSigner by default false
+ * @dev isSigner = true ---> provider
+ * @returns contract instance with type of obj: signer or provider
+ */
+
+export const getFactoryContract = (isSigner = false) => {
+  const provider = useProvider();
+  const { data: signer } = useSigner();
+
+  try {
+    const contract = useContract({
+      address: factoryAddress,
+      abi: factoryAbi,
+      signerOrProvider: isSigner ? signer : provider,
+    });
+
+    return contract;
+  } catch (e) {
+    console.log("unable to create contract instance", e);
+  }
 };
 
-export default useContract;
+export const getCampaignContract = (isSigner = false, deployedAddress) => {
+  const provider = useProvider();
+  const { data: signer } = useSigner();
+
+  try {
+    const contract = useContract({
+      address: deployedAddress,
+      abi: campaignAbi,
+      signerOrProvider: isSigner ? signer : provider,
+    });
+
+    return contract;
+  } catch (e) {
+    console.log("unable to create campaign contract instance", e);
+  }
+};
